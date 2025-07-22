4fc343bf0a6faece0f1c1b7569b58285ecb78b54
Sabiha Farhia Talukder <sabihatalukder6464@gmail.com>
2025-07-22 22:59:30 +0600

    add initial file

---
+++ usingSwitchStatemant.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int day = 5;
+    switch(day){
+        case 1:
+        cout << " Saturday ";
+        break;
+        case 2:
+        cout << " Sunday ";
+        break;
+        case 3:
+        cout << " Monday ";
+        break;
+        case 4:
+        cout << " Tuesday ";
+        break;
+        case 5:
+        cout << " Wensday ";
+        break;
+        case 6:
+        cout << " Thusday ";
+        break;
+        case 7:
+        cout << " Friday ";
+        break;
+
+    }
+    return 0;
+}
