2bcadce8e83ba002ac3d3595b6757147028ff162
Sabiha Farhia Talukder <sabihatalukder6464@gmail.com>
2025-07-23 20:59:13 +0600

    add initial file

---
+++ WhileLoopBasic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 3;
+    while(i > 0){
+        cout << i << endl;
+        i--;
+    }
+    cout << "Happy New Year" << endl;
+    return 0;
+}
